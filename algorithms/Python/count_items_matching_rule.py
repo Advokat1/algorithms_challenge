f9359c5c9f467d17a720e3eefc31b2606080b954
Dmitry Nazarov <DmitriyAdvokatNazarow@yandex.ru>
2021-05-09 15:03:42 +0300

    выполнил задачу

---
+++ algorithms/Python/count_items_matching_rule.py
@@ -0,0 +1,32 @@
+"""
+? Вам дан массив items, который состоит из элементов следующего вида:                           items[i] = [typei, colori, namei] где тип, цвет и название - пояснения раскрывающие данные об элементе.
+! На вход подается строка, состоящаяя из двух элементов:                                    ruleKey, ruleValue.
+* Необходимо посчитать количество элементов массива items, соответствующик указанным данным в переданной строке.
+---
+items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]
+ruleKey = "color", ruleValue = "silver"
+На выходе будет выдано: 1, так как всего одна запись в массиве соответствует правилу color = silver
+"""
+items = [
+    ["phone","blue","pixel"],
+    ["computer","silver","lenovo"],
+    ["phone","gold","iphone"]
+]
+ruleKey = input('Введите ключ')
+ruleValue = input('Введите значение')
+
+def count_items(items, ruleKey, ruleValue):
+    count = 0
+    rule = {
+        'type': 0,
+        'color': 1,
+        'name': 2,
+    }
+    for item in items:
+        if item[rule[ruleKey]] == ruleValue:
+            count+=1
+    
+    return count
+
+
+print(count_items(items, ruleKey, ruleValue))
